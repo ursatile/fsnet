62fa3cab5fc9ad26c67d22d20a93735d12576a83
Dylan Beattie <dylan@dylanbeattie.net>
2022-12-02 12:14:00 +0000

    DB context changes for domain model

--- dotnet/module09/Tikitapp/Tikitapp.Website/Data/TikitappDbContext.cs
+++ dotnet/module09/Tikitapp/Tikitapp.Website/Data/TikitappDbContext.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Tikitapp.Website.Data.Entities;
 
 namespace Tikitapp.Website.Data;
@@ -36,19 +38,21 @@ public class TikitappDbContext : DbContext {
 		});
 	}
 
-	// ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-	private bool IsSlug(PropertyInfo prop) => prop != null && prop.PropertyType == typeof(string) && prop.Name == "Slug";
+	private static bool IsSlug(IMutableProperty prop) {
+		var info = prop.PropertyInfo;
+		if (info == null) return false;
+		if (info.PropertyType != typeof(string)) return false;
+		return info.Name == "Slug";
+	}
 
-	private void ConfigureSlugs(ModelBuilder builder) {
-		var entities = builder.Model.GetEntityTypes();
-		var properties = entities.SelectMany(entity => entity.GetProperties());
-		var slugs = properties.Where(p => IsSlug(p.PropertyInfo));
-		Console.WriteLine(slugs);
-		foreach (var slug in slugs) {
-			Console.WriteLine(slug.DeclaringEntityType.Name);
-			slug.SetIsUnicode(false);
-			slug.SetMaxLength(100);
-			slug.IsIndex();
+	private static void ConfigureSlugs(ModelBuilder builder) {
+		foreach (var entity in builder.Model.GetEntityTypes()) {
+			var properties = entity.GetProperties();
+			foreach (var slug in properties.Where(IsSlug)) {
+				slug.SetIsUnicode(false);
+				slug.SetMaxLength(100);
+				entity.AddIndex(slug);
+			}
 		}
 	}
 }
